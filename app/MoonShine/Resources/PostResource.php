da1fead756740b2e2a8140900e43a19dd7d20fde
Thomas Clavijo <tsn.9682545@gmail.com>
2024-05-10 20:44:26 -0400

    primer commit

---
+++ app/MoonShine/Resources/PostResource.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
+use Illuminate\Support\Facades\Request;
+use MoonShine\Resources\ModelResource;
+use MoonShine\Decorations\Block;
+use MoonShine\Fields\ID;
+use MoonShine\Fields\Field;
+use MoonShine\Components\MoonShineComponent;
+use MoonShine\Fields\Text;
+
+/**
+ * @extends ModelResource<Post>
+ */
+class PostResource extends ModelResource
+{
+    protected string $model = Post::class;
+
+    protected string $title = 'Campamento Fuego de Dios';
+
+    protected bool $createInModal = true; 
+ 
+    protected bool $editInModal = true; 
+ 
+    protected bool $detailInModal = false;
+
+    public function scopes(): array
+    {
+        return [
+            new \App\Models\Scopes\ActivePostsScope(),
+        ];
+    }
+
+    public function redirectAfterSave(): string
+    {
+        $referer = Request::header('referer');
+        return $referer ?: '/';
+    }
+
+    /**
+     * @return list<MoonShineComponent|Field>
+     */
+    public function fields(): array
+    {
+        return [
+            Block::make([
+                ID::make()->sortable(),
+                Text::make('NOMBRE'),
+                Text::make('SEXO'),
+                Text::make('EDAD')
+            ]),
+        ];
+    }
+
+    /**
+     * @param Post $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    public function rules(Model $item): array
+    {
+        return [];
+    }
+}
